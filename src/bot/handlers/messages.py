02960d277582d74c2b02773747857458718a3085
3DZHio <3dzhio@gmail.com>
2024-10-30 09:28:16 +0200

    UPDATE

--- src/bot/handlers/messages.py
+++ src/bot/handlers/messages.py
@@ -1,21 +1,23 @@
-from aiogram import F
+from aiogram import Bot, F
 from aiogram.filters import CommandStart
+from aiogram.fsm.context import FSMContext
 from aiogram.types import Message
 
 from src.bot.texts import outer
-from src.bot.utils import routers
+from src.bot.utils import routers, extensions
 from src.database.models import users
 
 
 ### MAIN ###
 ## START ##
 @routers.msg.message(CommandStart())
-async def cmd_start(message: Message) -> None:
+async def cmd_start(message: Message, state: FSMContext, bot: Bot) -> None:
     uid = message.from_user.id
     await message.delete()
     if not await users.exists(uid):
         await users.add(uid)
-    await message.answer(text=outer.msg_start)
+    await extensions.delete_message("msg_start", uid, state, bot)
+    await state.update_data(msg_start=(await message.answer(text=outer.msg_start)).message_id)
 
 
 ### ADMIN ###
